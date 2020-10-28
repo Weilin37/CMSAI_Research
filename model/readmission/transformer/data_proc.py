ffb832f54b0a26f70a6429731c62c4d359bde5b1
EC2 Default User <ec2-user@ip-172-16-180-43.ec2.internal>
2020-10-28 02:05:19 +0000

    FEAT: data and modeling functions
    data_proc: functions to process input data
    dataset_func: functions to generate datasets (savable)
    model_func: functions to train and validate models
    transformer_model: model architecture definition

---
+++ model/readmission/transformer/data_proc.py
@@ -0,0 +1,160 @@
+import pandas as pd
+import numpy as np
+import os
+import pickle
+import numbers
+from collections import Counter
+import torch
+from torchtext.vocab import Vocab
+
+def read_data(
+    data_fp, drop_duplicates=False, check=True, y_target=None, uid=None, test=0
+):
+    """
+    Function to read-in, check and process raw 365 training or test data
+    
+    Arguments:
+    ----------
+        data_fp (str) : filepath to csv
+        drop_duplicates (bool) : drop duplicate UID (must provide uid)
+        check (bool) : check for data quality (nrows, duplicates, label ratio)
+                       must provide uid, y_target
+        y_target (str) : default None
+                         column name of label
+        uid (str) : default None
+                    column name of UID of dataset (usually patient_id + discharge_id)
+        test (int) : default 0
+                     if not zero, will read in test number of rows
+                    
+    Returns:
+    --------
+        data_df (dataframe) : cleaned dataframe
+    
+    """
+    if not os.path.isfile(data_fp):
+        raise Exception(f"Invalid data filepath: {data_fp}")
+
+    if test:
+        data_df = pd.read_csv(data_fp, low_memory=False, nrows=test)
+    else:
+        data_df = pd.read_csv(data_fp, low_memory=False)
+    print(f"Read data from {data_fp}\n")
+
+    num_dropped = 0
+    if drop_duplicates:
+        if uid not in data_df.columns:
+            raise Exception("Missing UID, unable to drop duplicates")
+
+        num_droppped = data_df.shape[0]
+        data_df.drop_duplicates(uid, inplace=True)
+        num_dropped -= data_df.shape[0]
+
+    if check:
+        print("=" * 20 + "Checking data" + "=" * 20 + "\n")
+        print(f"Data size: {data_df.shape}\n")
+
+        if drop_duplicates:
+            print(f"Number of duplicate rows: {num_dropped}\n")
+
+        if (y_target is not None) and (y_target in data_df.columns):
+            print(f"Label ratio for {y_target}")
+            print(data_df[y_target].value_counts(normalize=True))
+
+        if (uid is not None) and (uid in data_df.columns):
+            nb_duplicates = data_df[uid].duplicated().sum()
+            print(f"\nDischarge_id duplicates: {nb_duplicates}")
+
+    return data_df
+
+
+def remove_death(data_df, y_target, x_inputs, bad_word="death"):
+    """
+    Removes any rows in data that contains unwanted words, i.e. death
+    in any of the x_input columns
+    
+    Arguments:
+    ----------
+        data_df (dataframe) : data to process
+        x_inputs (list) : list of input columns to consider when removing words
+        y_target (str) : column name of label 
+        bad_word (str) : word used to decide which rows to remove
+        
+    Returns:
+    --------
+        data_df_str (dataframe) : data with rows containing unwanted words removed
+                    
+    
+    """
+    data_df_str = data_df.astype(str)
+    data_df_str[y_target] = data_df[y_target].astype(int).tolist()[:]
+
+    indices = set()
+    for input_col in x_inputs:
+        indices.update(data_df_str[data_df_str[input_col].str.contains(bad_word)].index)
+
+    print("\n" + "=" * 20 + "Removing bad word data" + "=" * 20 + "\n")
+    print(f"Removing bad words: {len(indices)} rows contain the word {bad_word}")
+
+    return data_df[~data_df.index.isin(indices)]
+
+def build_vocab(
+    data_df,
+    feat_colnames,
+    y_target="unplanned_readmission",
+    min_freq=1,
+    specials=["<pad>", "<unk>"],
+    pos_labs_vocab=True,
+):
+    """
+    Create a vocabulary: This maps all events to an index, including 
+    <pad> : index 0, sentence padding
+    <unk> : index 1, unknown events
+    nan : index 2, no events
+    
+    Arguments:
+    ----------
+        data_df (dataframe) : containing features & target
+        feat_colnames (list) : input column names to build vocab
+        y_target (str) : target column name
+        min_freq (int) : minimum frequency for vocab
+        specials (list) : special characters (padding, unknown)
+        pos_labs_vocab (bool) : to use only words from minority/pos class
+        
+    Returns:
+    --------
+        vocab (Vocab) 
+    """
+
+    def build_counter(data_df, feat_colnames):
+        counter = Counter()
+        words = data_df[feat_colnames].values.ravel("K")
+        print("start word number: ", words.shape)
+
+        new_words = []
+
+        for x in words:
+            x = str(x)
+            x = x.replace("d_s", "d_")
+            new_words.extend(x.split(", "))
+
+        print("exact word number: ", len(new_words))
+
+        counter.update(new_words)
+
+        if not isinstance(min_freq, numbers.Number):
+            raise ValueError(f"Something wrong with {min_freq}")
+
+        return counter
+
+    print("\n" + "=" * 20 + "Build vocabulary" + "=" * 20 + "\n")
+    vocab_df = data_df
+    if pos_labs_vocab:
+        vocab_df = data_df[data_df[y_target] == True]
+
+    counter = build_counter(vocab_df, feat_colnames)
+
+    vocab = Vocab(counter, min_freq=min_freq, specials=specials, specials_first=True)
+
+    print("Completed vocabulary")
+
+    return vocab
---
+++ model/readmission/transformer/transformer_model.py
@@ -0,0 +1,182 @@
+import math
+import torch
+from torch import nn
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+
+class PositionalEncoding(nn.Module):
+    """Positional encoding to be used in transfromer model class
+    """
+
+    def __init__(self, d_model, seq_length, dim, dropout=0.1, max_len=5000):
+        """
+        Arguments:
+        ----------
+            d_model : embedding size
+            seq_length : length of events
+            dim : num_events possible per day
+        """
+        super(PositionalEncoding, self).__init__()
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        # initialize positional encoding
+        pe = torch.zeros(max_len, d_model)
+
+        position = torch.tensor(
+            [float(i) for i in range(dim)] * seq_length
+            + [0.0] * (max_len - seq_length * dim),
+            dtype=torch.float,
+        ).unsqueeze(1)
+
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+
+        # standard positional encoding
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        x = x + self.pe[: x.size(0), :]
+        return self.dropout(x)
+    
+    
+class TransformerCNNModel(nn.Module):
+    def __init__(
+        self,
+        ntoken,
+        ninp,
+        nhead,
+        nhid,
+        nlayers,
+        num_classes,
+        device="cpu",
+        seq_length=366,
+        num_events=10,
+        dropout=0.5,
+    ):
+        """
+        Initialize a transformer model for hospital readmissions. The model consists of the following:
+        - Transformer encoder layers
+        - Single 1D CNN layer
+        - Final fully connected layer to determine probability of readmissions
+        
+        Args:
+        -----
+            ntoken : number of tokens in embedding layer (vocabulary size)
+            ninp : embedding dimension (number of inputs)
+            
+            nhead : number of heads in transformers
+            nhid : number of transformer linear dimensions
+            
+            nlayers : number of layers in transfromer
+            
+            num_classes : number of classes to predict (in this case, binary)
+            
+            seq_length : length of sequence in batched data
+            num_events : maximum number of events per day
+            
+            dropout : strength of regularization
+        """
+        super(TransformerCNNModel, self).__init__()
+        self.model_type = "Transformer"
+        self.device_type = device
+        
+        print(
+            "parameters: embsize:{}, nhead:{}, nhid:{}, nlayers:{}, dropout:{}".format(
+                ninp, nhead, nhid, nlayers, dropout
+            )
+        )
+
+        # Inputs into transformer: positional encoding and embeddings
+        self.pos_encoder = PositionalEncoding(ninp, seq_length, num_events, dropout)
+        self.seq_emb = nn.Embedding(ntoken, ninp)
+
+        # Transformer layer
+        encoder_layers = TransformerEncoderLayer(ninp, nhead, nhid, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, nlayers)
+
+        # CNN & fully connected layers
+        self.ff = nn.Linear(int(seq_length) * num_events, int(seq_length))
+        self.fc = nn.Linear(int(seq_length), num_classes)
+        self.softmax = nn.Softmax(-1)
+        self.Conv1d = nn.Conv1d(ninp, 1, 1, stride=1)
+
+        # record
+        self.ninp = ninp
+        self.dropout = dropout
+        self.num_events = num_events
+        self.num_classes = num_classes
+
+        # initalize weights
+        self.init_weights()
+
+    def seq_embedding(self, seq):
+        """Convert the sequence of events into embedding vectors, into single row per observation"""
+        batch, length_seq, dim = seq.size()
+        seq = seq.contiguous().view(batch * length_seq, dim)
+
+        seq = self.seq_emb(seq)
+
+        seq = seq.contiguous().view(batch, -1, self.ninp)
+
+        return seq
+
+    def init_weights(self):
+        """Initialize weights in embedding and fully connected layers"""
+        initrange = 0.1
+
+        self.seq_emb.weight.data.uniform_(-initrange, initrange)
+
+        self.fc.bias.data.zero_()
+        self.fc.weight.data.uniform_(-initrange, initrange)
+
+        self.ff.bias.data.zero_()
+        self.ff.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src, mask=None):
+        """
+        Forward propagation steps:
+        - convert events into embedding vectors & positional encoding
+        - transformer encoder layers
+        - CNN layer
+        - final 
+        """
+        # create mask to remove padded entries from calculations for interpretability
+        if mask is not None:
+            mask = mask.view(mask.size()[0], -1)
+            src_mask = mask == 0
+            src_mask = src_mask.view(src_mask.size()[0], -1)
+            out_mask = (
+                mask.float()
+                .masked_fill(mask == 0.0, float(-100.0))
+                .masked_fill(mask == 1.0, float(0.0))
+                .view(mask.size()[0], -1)
+            )
+
+        src = self.seq_embedding(src).transpose(0, 1) * math.sqrt(self.ninp)
+        src = self.pos_encoder(src)
+
+        trans_output = (
+            self.transformer_encoder(src, src_key_padding_mask=src_mask)
+            .transpose(0, 1)
+            .transpose(1, 2)
+        )
+
+        final_feature_map = self.Conv1d(trans_output).squeeze()
+
+        # if out_mask is not None:
+        # extract normalized feature importances per prediction
+        importance_out = self.softmax(final_feature_map + out_mask)
+
+        output = self.ff(final_feature_map)
+        output = self.fc(output)
+
+        # ensure no accidental additional dimensions
+        if len(output.size()) != 2:
+            output = output.view(1, 2)
+
+        return output, importance_out
